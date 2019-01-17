f4414d2a6443ba4e5c4e594a8c6ce21621c0eb67
NehirDemir <demirnehir2@gmail.com>
2019-01-17 22:10:24 +0300

    network icin genel hatlarıyla class olusturuldu,UDPSender guncellendi.

--- app/src/main/java/com/denizhan/intercom/Network/UDPSender.java
+++ app/src/main/java/com/denizhan/intercom/Network/UDPSender.java
@@ -7,4 +7,35 @@ package com.denizhan.intercom.Network;
 
 public class UDPSender {
 
+    // Alici adresi
+    private InetAddress local_address;
+
+    // Veri gonderme soketi
+    private DatagramSocket sendingSocket;
+
+    // Veri paketini tutmak için datagram paketi
+    private DatagramPacket sendingPacket;
+
+    public UDPSender(String ipAdress){
+         try {
+            sendingSocket = new DatagramSocket(4005);
+            local_address = InetAddress.getByName(ipAdress);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void send(byte[] data){
+
+        // paketi veri boyutuna göre guncelle
+        sendingPacket = new DatagramPacket(data, data.length, local_address, 4004);
+        try {
+            sendingSocket.send(sendingPacket); // sokete veri yollama
+        } catch (IOException e) {
+            e.printStackTrace();
+            // OlaGan hata paketin veri boyutunun udp buyuk olması
+           }
+    }
 }
