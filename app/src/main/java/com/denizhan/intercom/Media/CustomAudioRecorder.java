0359550ee59d3aa86c9871472d5590d62265e0ce
G. Uyaniker <gguyaniker@gmail.com>
2019-01-18 12:19:21 +0300

    Ses kaydedici metodları eklendi.

--- app/src/main/java/com/denizhan/intercom/Media/CustomAudioRecorder.java
+++ app/src/main/java/com/denizhan/intercom/Media/CustomAudioRecorder.java
@@ -1,10 +1,51 @@
 package com.denizhan.intercom.Media;
+import android.media.MediaRecorder;
+import com.external.test.Interfaces.ActivityMediaInteractionInterface;
+import java.io.IOException;
 
-/*
-    Yazacak Olan: Gizem
-    Açıklama: mp3 formatında ses kaydedici
-*/
+public class CustomAudioRecorder implements ActivityMediaInteractionInterface {
 
-public class CustomAudioRecorder {
+    private MediaRecorder recorder; // Android'in kendi medya kaydedicisi
 
+    public CustomAudioRecorder()
+    {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC); // Medya girişi olarak mikrofonu seç
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP); // Medya formatı olarak 3gp formatını kullan
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB); // AMR_NB (NarrowBand) ses kaydı kodeği, 8 bin Hz'de örnekleme alıyor.
+        // (Wideband 16 bin Hz'de örnekleme alıyor).
+        // Dosya boyutunu küçük tutmak için narrowband kullandım.
+    }
+
+    @Override
+    public void prepare()
+    {
+        recorder.setOutputFile("/storage/emulated/0/sample.3gp"); // Dosyanın kaydedileceği yer
+        try
+        {
+            recorder.prepare(); // Kayıt için hazırlan
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void start()
+    {
+        recorder.start(); // Kayda başla
+    }
+
+    @Override
+    public void stop()
+    {
+        recorder.stop(); // Kaydı durdur
+    }
+
+    @Override
+    public void destroy()
+    {
+        recorder.release(); // MediaRecorder objesini ve kullandığı kaynakları temizle
+    }
 }
