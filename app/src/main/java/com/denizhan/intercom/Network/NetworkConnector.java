22437a8a29f0a31b241693afccaf2e80247b7991
NehirDemir <demirnehir2@gmail.com>
2019-01-18 12:15:11 +0300

    Network icin thread yapisi olusturuldu.

--- app/src/main/java/com/denizhan/intercom/Network/NetworkConnector.java
+++ app/src/main/java/com/denizhan/intercom/Network/NetworkConnector.java
@@ -8,4 +8,76 @@ package com.denizhan.intercom.Network;
 
 public class NetworkConnector {
 
+    private UDPSender udpSender;
+    private UDPReceiver udpReceiver;
+
+    private Thread sendingThread, receivingThread;
+    private Runnable sendingRunnable, receivingRunnable;
+    private boolean sending,  receiving;
+
+    // baglanilacak ip adresini al
+
+    private NetworkConnector(String ipAdress){
+        this.udpSender = new UDPSender(ipAdress);
+        this.udpReceiver = new UDPReceiver();
+        initialize();
+    }
+
+    private void initialize(){
+        // veri gonderme metodunu sec
+        sendingRunnable = new Runnable() {
+
+            @Override
+            public void run() {
+                while(sending){
+                    send();
+                }
+            }
+        };
+        sendingThread = new Thread(sendingRunnable);
+        // veri alma metodunu sec
+        receivingRunnable = new Runnable() {
+            @Override
+            public void run() {
+                while(receiving){
+                    receive();
+                }
+            }
+        };
+        receivingThread = new Thread(receivingRunnable);
+    }
+
+    private void start(){
+        //veri gondermeye basla
+        sending = true;
+        sendingThread.start();
+
+        //veri almaya basla
+        receiving = true;
+        receivingThread.start();
+    }
+
+    private void stop(){
+        sending = false;
+        receiving = false;
+    }
+
+    private void destroy(){
+        sending = false;
+        sendingThread = null;
+
+        receiving = false;
+        receivingThread = null;
+    }
+
+    private void send(){
+      //buralara gonderme yapilacak
+    }
+
+    private void receive(){
+     //alma islemleri yapilacak
+    }
+
+
+
 }
